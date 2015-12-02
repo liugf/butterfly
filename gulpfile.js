0c8fb34ab8a3f895d051f9bfbb35e6bcc1e34efe
lorienliu <lorienliu@griffin.local>
2015-12-02 16:06:37 +0800

    modify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,5 @@
 var elixir = require('laravel-elixir');
 
-elixir.config.sourcemaps = true;
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
     mix.less('common/**/*.less', 'public/css/common.css')
         .less('articles/**/*.less', 'public/css/articles.css')
