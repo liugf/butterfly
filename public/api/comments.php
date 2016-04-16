b7a6f98d681bee288bb3374f57b74295c8beda20
liugf <liugf@vip.qq.com>
2016-04-16 11:47:32 +0800

    clean

--- public/api/comments.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$comments = [
-    ['id' => 1, 'author' => 'lorienliu', 'content' => 'hello'],
-    ['id' => 2, 'author' => 'cattyhuang', 'content' => 'world']
-];
-
-echo json_encode($comments);
